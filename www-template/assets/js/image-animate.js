b32d70a498e34ac011d9ee5e2a95c8076e8710fe
Toshiya Takeshita <toshi@atbat.jp>
2024-02-17 08:59:40 +0900

    JavaScriptのバグを修正

--- www-template/assets/js/image-animate.js
+++ www-template/assets/js/image-animate.js
@@ -1,7 +1,7 @@
 const faseSpeed = 5000;
 
 setTimeout(() => {
-    let whaleAndFisherman = document.querySelector("#whale-and-fisherman");
+    let whaleAndFisherman = document.querySelector("#kayac-and-fisherman");
     whaleAndFisherman.animate(
         [{opacity: '1.0'}, {opacity: '0.4'}],
         {duration: 1000, fill: 'forwards'}
